e60b04fc1855c7522d37586268c9c779fd2e690d
AbdulovADA <lil.vlad7608@gmail.com>
2022-04-24 20:03:29 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketAmount = 15375;
+        int miles = 20;
+        int milesBonus = ticketAmount / miles;
+        System.out.println("количество бонусных миль =" + milesBonus);
+    }
+}
